c9a1fe57433d1aa9ab5d621ef8144901777148be
Abhimukth Chaudhuri <abhimukth.chaudhuri@mail.mcgill.ca>
2019-08-23 02:40:24 -0400

    Refine exceptions and refactor code into functions

--- localNav.py
+++ localNav.py
@@ -2,23 +2,35 @@ import sys
 import os
 from github import Github, GithubException
  
-
-# Remove email and password credentials before committing to Git
+# Enter Github login credentials here
 username = "..."
 password = "..."
 
+# Enter workspace directory here with '\' at the end of path 
 workspace = "C:\\Users\\abhi2\\PythonLearningCurve\\"
-status = os.makedirs(workspace+sys.argv[1])
-# Access user account and make repo online
-try:
-    user = Github(username, password).get_user()
-    if user.create_repo(sys.argv[1]): #Repo will be created on Github through conditional check here 
-        print(f"Successfully created remote repository - {sys.argv[1]} - on Github")
-    else:
-        print(f"Unable to create remote repository {sys.argv[1]}") 
 
-except GithubException:
-    print("Exception Caught") # TODO: Change to see how specific messages can be displayed to the terminal 
+def create_project():
+    # Access user account and make repo online
+    try:
+        status = os.makedirs(workspace+sys.argv[1])
+        user = Github(username, password).get_user()
+        if user.create_repo(sys.argv[1]): #Repo will be created on Github through conditional check here 
+            print(f"Successfully created local directory in workspace and remote repository - {sys.argv[1]} - on Github")
+        else:
+            print(f"Unable to create remote repository {sys.argv[1]}") 
 
+    except FileExistsError:
+        print("Project with same name exists on system workspace directory.\nPlease try again.")
 
+    except GithubException as e:
+        if e.status == 422:
+            print("Repository with same name already exists on this account.\nPlease try again.")
+        elif e.status == 401:
+            print("Invalid login credentials. Please check username or password specified in localNav script.")
+
+        # Following line will only execute if Github error occurs but, the directory was still made in workspace directory
+        os.rmdir(workspace+sys.argv[1])
+
+if __name__ == "__main__":
+    create_project()
 
